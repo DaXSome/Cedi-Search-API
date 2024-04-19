6584abaad6196f0e6d3b068caac8c659497f1f64
owbird <owbirdphorccuo@gmail.com>
2024-04-19 09:23:44 +0000

    feat: Add search

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from json import load
 from rank_bm25 import BM25Okapi
+from algoliasearch.search_client import SearchClient
+from dotenv import load_dotenv
+from os import getenv
+
+
+load_dotenv()
 
 
 app = FastAPI()
@@ -26,6 +32,11 @@ tokenized_corpus = [doc.lower().split(" ") for doc in corpus]
 
 bm25 = BM25Okapi(tokenized_corpus)
 
+client = SearchClient.create(
+    getenv("ALGOLIA_APP_ID"), getenv("ALGOLIA_API_KEY"))
+
+index = client.init_index("products")
+
 
 @app.get("/")
 def read_root(query: str = Query()):
@@ -35,3 +46,20 @@ def read_root(query: str = Query()):
     suggestions = bm25.get_top_n(tokenized_query, corpus, n=10)
 
     return suggestions
+
+
+@app.get("/search")
+def get_search(query: str = Query()):
+    products = []
+
+    for product in index.search(query)["hits"]:
+
+        products.append({
+            "name": product["name"],
+            "price": float(product["price"]),
+            "rating": product["rating"],
+            "image": product["images"][0],
+            "id": product["product_id"]
+        })
+
+    return products
